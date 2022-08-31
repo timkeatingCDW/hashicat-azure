17c712768848c281013ec4edac79be77a7e93ddf
root <tim.keating@cdw.com>
2022-08-31 17:51:55 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/TKazure/network/azurerm"
   version = "3.5.0"
 
-  resource_group_name = "myresourcegroup"
+  resource_group_name = "${var.prefix}-workshop"
 }
